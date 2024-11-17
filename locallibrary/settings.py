b3ef48fbd9d45c8d2e1f89cd74e95476734318d1
digi10ve <digi10ve23@gmail.com>
2024-11-17 14:37:44 +0800

    allow python anywhere

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['digi10ve.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -157,3 +157,10 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://digi10ve.pythonanywhere.com']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
